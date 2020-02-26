8a902ef37795327806f12cad3278c773c95385dd
Crisescode <zhaopanp2018@outlook.com>
2020-02-27 00:04:26 +0800

    add spend times

--- str/1. two_sum.py
+++ str/1. two_sum.py
@@ -16,14 +16,32 @@ class Solution:
 
 
 class Solution2:
-    # Time:
-    # Space:
+    # Time: o(n)
+    # Space: o(1)
     def twoSum(self, nums, target):
-        pass
+        nums_dict = {}
+        for i, value in enumerate(nums):
+            tmp = target - value
+            if tmp in nums_dict:
+                return [nums_dict[tmp], i]
+            nums_dict[value] = i
+        return None
 
 
 if __name__ == "__main__":
+    import time
+    start_time1 = time.time()
     solu_1 = Solution()
     result = solu_1.twoSum([2, 4, 1, 0, 5], 2)
+    end_time1 = time.time()
     assert result == [0, 3]
     print(result)
+    print('solution 1 spend times: %d' % (end_time1 - start_time1))
+
+    start_time2 = time.time()
+    solu_2 = Solution2()
+    result = solu_2.twoSum([2, 4, 1, 0, 5], 2)
+    end_time2 = time.time()
+    # assert result == [0, 3]
+    print(result)
+    print('solution 1 spend times: %d' % (end_time2 - start_time2))
