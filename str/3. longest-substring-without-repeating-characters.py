d15a25b225d70c1846b56638bad5b601f8c59103
zhaopp5 <zhaopp5@lenovo.com>
2020-02-26 23:10:01 +0800

    add the first problem and modify .gitingore

---
+++ str/3. longest-substring-without-repeating-characters.py
@@ -0,0 +1,29 @@
+# Time:  O(n)
+# Space: O(1)
+#
+# Given a string, find the length of the longest substring without repeating characters.
+# For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3.
+# For "bbbbb" the longest substring is "b", with the length of 1.
+#
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        st = {}
+        res, i = 0, 0
+
+        for j in range(len(s)):
+            # move start flag
+            if s[j] in st:
+                i = max(i, st[s[j]])
+
+            res = max(res, j - i + 1)
+            st[s[j]] = j + 1
+
+        return res
+
+
+if __name__ == '__main__':
+    solu = Solution()
+    reslut = solu.lengthOfLongestSubstring('abcabcbb')
+    print(reslut)
