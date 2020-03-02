42ad58435529afd4b6c6436afd892d06ba8e3271
Crisescode <zhaopanp2018@outlook.com>
2020-03-02 22:56:06 +0800

    add maximum subarray

--- Array/53. maximum_subarray.py
+++ Array/53. maximum_subarray.py
@@ -13,9 +13,23 @@ from utils.timer_decorater import timer
 
 
 class Solution(object):
+    @timer
     def maxSubArray(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        pass
+        maxSum = nums[0]
+        maxContinuousSum = 0
+        for num in nums:
+            if maxContinuousSum > 0:
+                maxContinuousSum += num
+            else:
+                maxContinuousSum = num
+            maxSum = max(maxContinuousSum, maxSum)
+        return maxSum
+
+
+if __name__ == "__main__":
+    nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    print(Solution().maxSubArray(nums))
