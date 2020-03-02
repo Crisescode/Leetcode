544d42bacf975f2f43045d94f24933e9ba01335c
Crisescode <zhaopanp2018@outlook.com>
2020-03-02 22:40:59 +0800

    add maximum subarray

---
+++ Array/53. maximum_subarray.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# https://leetcode-cn.com/problems/maximum-subarray/
+# Given an integer array nums, find the contiguous subarray (containing at least one number)
+# which has the largest sum and return its sum.
+# For example, Given Input = [-2, 1, -3, 4, -1, 2, 1, -5, 4],
+# Output will be: 6, Because [4,-1,2,1] has the largest sum = 6.
+# If you have figured out the O(n) solution, try coding another solution using the divide
+# and conquer approach, which is more subtle.
+#
+
