b43f11a13a78006d61b6af321c6c8b40b98745ee
Crisescode <zhaopanp2018@outlook.com>
2020-03-06 22:50:47 +0800

    add parlindrome linked list

--- LinkedList/234. palindrome_linked_list.py
+++ LinkedList/234. palindrome_linked_list.py
@@ -32,6 +32,49 @@ class Solution(object):
         return data == data[::-1]
 
 
+class Solution2(object):
+    @timer
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head is None:
+            return True
+
+        slow_point = self.get_slowly_point(head)
+        reverse_point = self.reverse_linked_list(slow_point.next)
+
+        res = True
+        first_position = head
+        second_position = reverse_point
+        while res and second_position is not None:
+            if first_position.val != second_position.val:
+                res = False
+            first_position = first_position.next
+            second_position = second_position.next
+
+        return res
+
+    def get_slowly_point(self, head):
+        slow, fast = head, head
+        while fast.next is not None and fast.next.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+    def reverse_linked_list(self, head):
+        curr, prev = head, None
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+
+        return prev
+
+
 if __name__ == '__main__':
     l3 = l3_1 = ListNode(0)
     l3_1.next = ListNode(1)
@@ -47,4 +90,5 @@ if __name__ == '__main__':
     l3_1.next = ListNode(0)
 
     print(Solution().isPalindrome(l3))
+    print(Solution2().isPalindrome(l3))
 
