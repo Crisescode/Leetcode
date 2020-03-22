062ede0afaa6a318b21e53017b20f13e15ac3690
Crisescode <zhaopanp2018@outlook.com>
2020-03-22 21:34:28 +0800

    add vaild bracket

--- Stack/20. vaild_bracket.py
+++ Stack/20. vaild_bracket.py
@@ -1,5 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# https://leetcode-cn.com/problems/valid-parentheses/
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+# An input string is valid if:
+# 1. Open brackets must be closed by the same type of brackets.
+# 2. Open brackets must be closed in the correct order.
+# Note that an empty string is also considered valid.
+
+# For example, Given Input = "()[{}]", output will be True. Given Input = "([})", Output will be False.
+#
+from utils.timer_decorater import timer
+
 
 class Solution(object):
+    @timer
     def isValid(self, s):
         """
         :type s: str
