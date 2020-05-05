9fc05688aee4a2b5fb835a60fb8fd10142536892
Crisescode <zhaopanp2018@outlook.com>
2020-05-05 22:16:08 +0800

    [String] update longest palindromic substring

--- String/5. longest-palindromic-substring.py
+++ String/5. longest-palindromic-substring.py
@@ -1,3 +1,19 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# https://leetcode-cn.com/problems/longest-palindromic-substring/
+# Given a string s, find the longest palindromic substring in s. You may assume that the maximum
+# length of s is 1000.
+#
+
+# For example:
+'''
+Example 1:
+    Input: "babad"
+    Output: "bab"
+    Note: "aba" is also a valid answer.
+
+Example 2:
+    Input: "cbbd"
+    Output: "bb
+'''
