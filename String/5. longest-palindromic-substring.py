d1f310c34b30a3da7aa6d85a8890708114e7f99e
Crisescode <zhaopanp2018@outlook.com>
2020-05-06 23:22:13 +0800

    add solution2 to 5

--- String/5. longest-palindromic-substring.py
+++ String/5. longest-palindromic-substring.py
@@ -44,10 +44,42 @@ class Solution:
         return True
 
 
+# 中心扩散法：
 class Solution2:
-    pass
+    @timer
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) < 2:
+            return s
+
+        length = len(s)
+        left, right, maxLen, maxStart = 0, 0, 0, 0
+        lens = 1
+
+        for i in range(length):
+            left = i - 1
+            right = i + 1
+
+            while left >= 0 and s[left] == s[i]:
+                left -= 1
+                lens += 1
+            while right < length and s[right] == s[i]:
+                right += 1
+                lens += 1
+            while left >= 0 and right < length and s[left] == s[right]:
+                lens = lens + 2
+                left -= 1
+                right += 1
+
+            if lens > maxLen:
+                maxLen = lens
+                maxStart = left
+
+            lens = 1
+
+        return s[maxStart + 1:maxStart + maxLen + 1]
 
 
 if __name__ == "__main__":
     s = "abcscbmsadd"
     print(Solution().longestPalindrome(s))
+    print(Solution2().longestPalindrome(s))
