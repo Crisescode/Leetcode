46858fdd318d068d6004088bfe76d68895121382
Crisescode <zhaopanp2018@outlook.com>
2020-05-11 23:15:09 +0800

    update merged tree

--- Tree/617. merge_two_binary_trees.py
+++ Tree/617. merge_two_binary_trees.py
@@ -2,7 +2,32 @@
 # -*- coding: utf-8 -*-
 
 
-# Definition for a binary tree node.
+# https://leetcode-cn.com/problems/merge-two-binary-trees/
+# Given two binary trees and imagine that when you put one of them to cover the other,
+# some nodes of the two trees are overlapped while the others are not.
+# You need to merge them into a new binary tree. The merge rule is that if two nodes overlap,
+# then sum node values up as the new value of the merged node. Otherwise,
+# the NOT null node will be used as the node of new tree.
+#
+"""
+Example:
+    Input:
+              Tree 1                 Tree2
+                 1                     2
+               / \                    / \
+             3    2                  1   3
+           /                         \    \
+         5                            4    7
+    Output:
+    Merged tree:
+                          3
+                         / \
+                       4    5
+                      / \    \
+                    5    4    7
+"""
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
