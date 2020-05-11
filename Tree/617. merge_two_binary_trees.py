63cee63c8a4d2e5984242ecd3615bccb84bf1f49
Crisescode <zhaopanp2018@outlook.com>
2020-05-11 23:36:25 +0800

    update merged tree

--- Tree/617. merge_two_binary_trees.py
+++ Tree/617. merge_two_binary_trees.py
@@ -44,5 +44,35 @@ class Solution:
         root = TreeNode(t1.val + t2.val)
         root.left = self.mergeTrees(t1.left, t2.left)
         root.right = self.mergeTrees(t1.right, t2.right)
-        return root
+
+        setattr(self, "_root", root)
+
+        # return root
+
+    def preOrder(self):
+        if self._root == None:
+            return
+
+        print(self._root.val, end=' ')
+        self.preOrder(self._root.left)
+        self.preOrder(self._root.right)
+
+
+if __name__ == "__main__":
+    tree1 = TreeNode(1)
+    tree1.left = TreeNode(3)
+    tree1.right = TreeNode(2)
+    tree1.left.left = TreeNode(5)
+
+    print(tree1.left.left.val)
+
+    tree2 = TreeNode(2)
+    tree2.left = TreeNode(1)
+    tree2.right = TreeNode(3)
+    tree2.left.right = TreeNode(4)
+    tree2.right.right = TreeNode(7)
+    print(tree2.left.right.val)
+
+    Solution().mergeTrees(tree1, tree2)
+    Solution().preOrder()
 
