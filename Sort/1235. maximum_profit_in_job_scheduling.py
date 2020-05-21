943a2fc6e76610bf646973b3647c678e756c413e
Crisescode <zhaopanp2018@outlook.com>
2020-05-21 15:57:41 +0800

    update 1235 maximum profit in job scheduling

--- Sort/1235. maximum_profit_in_job_scheduling.py
+++ Sort/1235. maximum_profit_in_job_scheduling.py
@@ -12,6 +12,14 @@
 # If you choose a job that ends at time X you will be able to start
 # another job that starts at time X.
 
+# For example:
+# Input: startTime = [1, 2, 3, 4], endTime = [3, 4, 5, 6],
+# profit = [50, 10, 40, 70]
+# Output: 120
+# Explanation: The subset chosen is the first and fourth job.
+# Time range [1-3]+[3-6] , we get profit of 120 = 50 + 70.
+
+
 from typing import List
 
 
