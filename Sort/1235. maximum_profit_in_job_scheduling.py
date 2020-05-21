e95c7622abc2ba1c18f028aa70609cceb5c5c657
Crisescode <zhaopanp2018@outlook.com>
2020-05-21 19:08:40 +0800

    update 1235 maximum profit in job scheduling

--- Sort/1235. maximum_profit_in_job_scheduling.py
+++ Sort/1235. maximum_profit_in_job_scheduling.py
@@ -30,4 +30,55 @@ class Solution:
             endTime: List[int],
             profit: List[int]
     ) -> int:
-        pass
+        profit_time_range = []
+
+        for index in range(len(profit)):
+            start = startTime[index]
+            end = endTime[index]
+            pay = profit[index]
+            profit_time_range.append((start, end, pay))
+
+        print(profit_time_range)
+
+        count = []
+        for i in range(len(profit_time_range)):
+            for j in range(i, len(profit_time_range)):
+                if profit_time_range[j][0] >= profit_time_range[i][1]:
+                    count.append((profit_time_range[i], profit_time_range[j]))
+        print(count)
+
+
+if __name__ == "__main__":
+    start_time = [1, 2, 3, 4, 6]
+    end_time = [3, 5, 10, 6, 9]
+    profit = [20, 20, 100, 70, 60]
+    import itertools
+
+    # H = sorted(zip(start_time, itertools.repeat(1), end_time, profit))
+    # print(H)
+    # res = 0
+    # import heapq
+    # while H:
+    #     t = heapq.heappop(H)
+    #     print(t)
+    #     if t[1]:
+    #         heapq.heappush(H, (t[2], 0, res + t[3]))
+    #     else:
+    #         res = max(res, t[2])
+    # print(res)
+    import bisect
+    jobs = sorted(zip(start_time, end_time, profit), key=lambda v: v[1])
+    print(jobs)
+    dp = [[0, 0]]
+    for s, e, p in jobs:
+        # bisect.insort(dp, [s + 1])
+        # print(dp)
+        i = bisect.bisect(dp, [s + 1]) - 1
+        print(i)
+        print(dp)
+        if dp[i][1] + p > dp[-1][1]:
+            dp.append([e, dp[i][1] + p])
+
+    # print(dp[-1][1])
+
+    # Solution().jobScheduling(start_time, end_time, profit)
