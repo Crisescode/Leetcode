69f01f39c7c50af1788183cd98b078cef9292c4e
Crisescode <zhaopanp2018@outlook.com>
2020-05-28 12:51:43 +0800

    add tree

---
+++ 剑指offer/二叉搜索树的后序遍历序列.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+题目描述：
+输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。
+如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
+
+时间复杂度： O(n)
+空间复杂度： O(n)
+"""
+
+
+class Solution:
+    def VerifySquenceOfBST(self, sequence):
+        # write code here
+        pass
