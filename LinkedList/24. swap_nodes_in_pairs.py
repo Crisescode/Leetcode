2b293550ab0e8249defba338d2c115589f8b7051
Crisescode <zhaopanp2018@outlook.com>
2020-07-02 15:13:34 +0800

    add 24. swap nodes in pairs

---
+++ LinkedList/24. swap_nodes_in_pairs.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+# Given a linked list, swap every two adjacent nodes and return its head.
+# You may not modify the values in the list's nodes, only nodes itself may be changed.
+# For example, Given Input: 1->2-3->4, Output will be: 2->1->4->3.
+#
+from utils.timer_decorater import timer
+from linked_utils.node import ListNode
+
+
+class Solution:
+    @timer
+    def swapPairs(self, head: ListNode) -> ListNode:
+        pass
