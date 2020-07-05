ab859fa6ff1f1a066313be1b418cdea7913c71a0
Crisescode <zhaopanp2018@outlook.com>
2020-07-05 20:43:00 +0800

    add JZ51

---
+++ 剑指offer/JZ51 构建乘积数组.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: Crise
+# date: 05/07/20
+
+"""
+题目描述：
+给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。
+不能使用除法。（注意：规定B[0] = A[1] * A[2] * ... * A[n-1]，B[n-1] = A[0] * A[1] * ... * A[n-2];）
+
+时间复杂度： O(n)
+空间复杂度： O(n)
+"""
+
+
+class Solution:
+    def multiply(self, A):
+        # write code here
+        pass
