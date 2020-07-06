764aa7e22ee1de0f7e5fb6ffb392923d766c7055
Crisescode <zhaopanp2018@outlook.com>
2020-07-06 10:13:16 +0800

    update JZ3 & JZ15

---
+++ 剑指offer/JZ3 从尾到头打印链表.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: Crise
+# date: 06/07/20
+
+"""
+题目描述：
+输入一个链表，按链表从尾到头的顺序返回一个ArrayList。
+
+时间复杂度： O(n)
+空间复杂度： O(n)
+"""
+
+
+class Solution:
+    # 返回从尾部到头部的列表值序列，例如[1,2,3]
+    def printListFromTailToHead(self, listNode):
+        # write code here
+        pass
