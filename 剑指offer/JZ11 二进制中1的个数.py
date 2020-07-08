c738795b9a3d8f05102d7d4437853501a5c0cff7
Crisescode <zhaopanp2018@outlook.com>
2020-07-08 13:26:46 +0800

    add JZ47

---
+++ 剑指offer/JZ11 二进制中1的个数.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: Crise
+# date: 08/07/20
+
+"""
+题目描述：
+输入一个整数，输出该数32位二进制表示中1的个数。其中负数用补码表示。
+
+"""
+
+
+class Solution:
+    def NumberOf1(self, n):
+        # write code here
+        pass
