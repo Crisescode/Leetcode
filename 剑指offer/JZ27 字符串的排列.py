e6f46ce8a9b4b178901803079b0747df73520f19
Crisescode <zhaopanp2018@outlook.com>
2020-07-08 10:39:04 +0800

    add JZ27

---
+++ 剑指offer/JZ27 字符串的排列.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: Crise
+# date: 06/07/20
+
+"""
+题目描述：
+输入一个字符串,按字典序打印出该字符串中字符的所有排列。例如输入字符串abc,
+则打印出由字符 a,b,c 所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+
+时间复杂度： O(n)
+空间复杂度： O(1)
+"""
+
+
+class Solution:
+    def Permutation(self, ss):
+        # write code here
+        pass
+
