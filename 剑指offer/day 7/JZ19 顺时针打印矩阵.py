fb1573aed4cab97f66827e095f79d4ce13277381
Crisescode <zhaopanp2018@outlook.com>
2020-07-19 22:12:30 +0800

    update JZ19

--- 剑指offer/day 7/JZ19 顺时针打印矩阵.py
+++ 剑指offer/day 7/JZ19 顺时针打印矩阵.py
@@ -15,19 +15,30 @@ class Solution:
     # matrix类型为二维列表，需要返回列表
     def printMatrix(self, matrix):
         # write code here
-        if len(matrix) == 1:
-            return matrix[0][0]
+        top = 0
+        bottom = len(matrix)
+        left = 0
+        right = len(matrix[0])
 
-        row_min = 0
-        row_max = len(matrix)
-        col_min = 0
-        col_max = len(matrix[0])
+        res = []
+        while top < bottom and left < right:
+            # 向右
+            res.extend([matrix[top][c] for c in range(left, right)])
+            # 向下
+            res.extend([matrix[r][right - 1] for r in range(top + 1, bottom)])
+            # 向左
+            if bottom - top > 1:
+                res.extend([matrix[bottom - 1][c] for c in range(right - 2, left, -1)])
+            # 向上
+            if right - left > 1:
+                res.extend([matrix[r][left] for r in range(bottom - 1, top, -1)])
 
-        # 向右
-        res = [matrix[0][j] for j in range(col_max)]
-        res.extend([matrix[i][col_max - 1] for i in range(row_min + 1, row_max)])
-        res.extend([matrix[row_max - 1][j] for j in range()])
-        print(res)
+            top += 1
+            bottom -= 1
+            left += 1
+            right -= 1
+
+        return res
 
 
 if __name__ == "__main__":
@@ -37,6 +48,6 @@ if __name__ == "__main__":
         for j in range(4):
             matrix[i][j] = num
             num += 1
-    print(matrix)
 
     print(Solution().printMatrix(matrix))
+    print(Solution().printMatrix([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]))
