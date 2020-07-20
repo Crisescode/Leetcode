9d542ddb64d20be92e1fa7e8e104a769dfb61b93
Crisescode <zhaopanp2018@outlook.com>
2020-07-20 22:32:41 +0800

    add JZ 60

---
+++ 剑指offer/day 8/JZ60 把二叉树打印成多行.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: Crise
+# date: 20/07/20
+
+"""
+题目描述：
+从上到下按层打印二叉树，同一层结点从左至右输出。每一层输出一行。
+
+"""
+
+
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回二维列表[[1,2],[4,5]]
+    def Print(self, pRoot):
+        # write code here
+        pass
+
+
+if __name__ == "__main__":
+    print()
