375fc308e36251f26681be0e0aac85d903d7057f
Crisescode <zhaopanp2018@outlook.com>
2020-07-20 23:29:17 +0800

    add JZ58

---
+++ 剑指offer/day 8/JZ58 对称的二叉树.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: Crise
+# date: 20/07/20
+
+"""
+题目描述：
+请实现一个函数，用来判断一棵二叉树是不是对称的。注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
+
+"""
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def isSymmetrical(self, pRoot):
+        # write code here
+        pass
