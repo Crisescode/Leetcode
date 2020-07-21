b6bf2c6dfc38b128d2e49247f91e36eb780d4edd
Crisescode <zhaopanp2018@outlook.com>
2020-07-21 23:12:06 +0800

    add JZ39

---
+++ 剑指offer/day 8/JZ39 平衡二叉树.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: Crise
+# date: 20/07/20
+
+"""
+题目描述：
+输入一棵二叉树，判断该二叉树是否是平衡二叉树。
+
+在这里，我们只需要考虑其平衡性，不需要考虑其是不是排序二叉树
+
+"""
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def IsBalanced_Solution(self, pRoot):
+        # write code here
+        pass
