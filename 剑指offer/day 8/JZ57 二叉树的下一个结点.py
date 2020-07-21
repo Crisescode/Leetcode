56639e508e6178e8c2a5d147f74e753c818f17a1
Crisescode <zhaopanp2018@outlook.com>
2020-07-21 23:07:35 +0800

    add JZ57

---
+++ 剑指offer/day 8/JZ57 二叉树的下一个结点.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: Crise
+# date: 20/07/20
+
+"""
+题目描述：
+给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
+
+"""
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def GetNext(self, pNode):
+        # write code here
+        pass
