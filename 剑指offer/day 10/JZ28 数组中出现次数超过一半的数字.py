b31ffb5837ac9b953fe25913e6814c234f0b8524
Crisescode <zhaopanp2018@outlook.com>
2020-07-28 21:57:01 +0800

    add JZ28

---
+++ 剑指offer/day 10/JZ28 数组中出现次数超过一半的数字.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: Crise
+# date: 27/07/20
+
+"""
+题目描述：
+输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4。
+
+"""
+
+
+class Solution:
+    def MoreThanHalfNum_Solution(self, numbers):
+        # write code here
+        pass
+
+
+if __name__ == "__main__":
+    pass
