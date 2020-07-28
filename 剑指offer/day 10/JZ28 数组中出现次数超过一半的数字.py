83fddb0eba72b1a6399edffd7eaf38eadd13f1d8
Crisescode <zhaopanp2018@outlook.com>
2020-07-28 22:04:49 +0800

    update JZ28

--- 剑指offer/day 10/JZ28 数组中出现次数超过一半的数字.py
+++ 剑指offer/day 10/JZ28 数组中出现次数超过一半的数字.py
@@ -5,7 +5,8 @@
 
 """
 题目描述：
-输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4。
+数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。
+由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
 
 """
 
@@ -13,8 +14,20 @@
 class Solution:
     def MoreThanHalfNum_Solution(self, numbers):
         # write code here
-        pass
+        if len(numbers) == 0:
+            return 0
+
+        length = len(numbers)
+        hash_map = {}
+        for num in numbers:
+            if num in hash_map:
+                hash_map[num] += 1
+            else:
+                hash_map[num] = 1
+        nums = [num for num in hash_map.keys() if hash_map[num] > length / 2]
+        return nums[0] if nums else 0
 
 
 if __name__ == "__main__":
-    pass
+    numbers = [1, 2, 3, 2, 2, 2, 5, 4, 2]
+    print(Solution().MoreThanHalfNum_Solution(numbers))
