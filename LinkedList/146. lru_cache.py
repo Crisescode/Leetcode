b329fb9af586ff211e23d002903643cc12c738e3
Crisescode <zhaopanp2018@outlook.com>
2020-09-12 20:52:10 +0800

    update 146

--- LinkedList/146. lru_cache.py
+++ LinkedList/146. lru_cache.py
@@ -43,23 +43,70 @@ lRUCache.get(4);    // return 4
 
 """
 
-from utils.node import ListNode
-from Utils.timer_decorater import timer
+
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
 
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        pass
+        self.dict = {}
+        self.capacity = capacity
+        self.dummy_head = ListNode(0, 0)
+        self.dummy_tail = ListNode(0, 0)
+        self.dummy_head.next = self.dummy_tail
+        self.dummy_tail.prev = self.dummy_head
 
     def get(self, key: int) -> int:
-        pass
+        if key not in self.dict:
+            return -1
+
+        node = self.dict[key]
+        self.remove(node)
+        self.append(node)
+
+        return node.val
 
     def put(self, key: int, value: int) -> None:
-        pass
+        if key in self.dict:
+            self.remove(self.dict[key])
+
+        node = ListNode(key, value)
+        self.append(node)
+        self.dict[key] = node
+
+        if len(self.dict) > self.capacity:
+            head = self.dummy_head.next
+            self.remove(head)
+            del self.dict[head.key]
+
+    def remove(self, node: ListNode):
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
+
+    def append(self, node: ListNode):
+        tail = self.dummy_tail.prev
+        tail.next = node
+        node.prev = tail
+        self.dummy_tail.prev = node
+        node.next = self.dummy_tail
 
 
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+if __name__ == "__main__":
+    obj = LRUCache(2)
+    print(obj.get(1))
+    obj.put(1, 1)
+    obj.put(2, 2)
+    print(obj.get(1))
+    obj.put(3, 3)
+    print(obj.get(2))
+    obj.put(4, 4)
+    print(obj.get(1))
+    print(obj.get(3))
