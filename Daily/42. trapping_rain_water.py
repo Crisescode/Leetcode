bf2566570af829a6fea3b1054e11e467c4a68dd1
Crisescode <zhaopanp2018@outlook.com>
2020-09-13 10:42:43 +0800

    [leetcode] update 42

--- Daily/42. trapping_rain_water.py
+++ Daily/42. trapping_rain_water.py
@@ -11,71 +11,84 @@
 # Input: [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 # Output: 6
 #
+from typing import List
 
 
-class ListNode:
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        self.next = None
-        self.prev = None
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if len(height) <= 1:
+            return 0
+
+        res = 0
+        for i in range(len(height)):
+            max_left, max_right = 0, 0
+            for j in range(0, i):
+                max_left = max(max_left, height[j])
+
+            for j in range(i, len(height)):
+                max_right = max(max_right, height[j])
+
+            if min(max_left, max_right) > height[i]:
+                res += min(max_left, max_right) - height[i]
+
+        return res
 
 
-class LRUCache:
+# 备忘录方法，提前计算好左右最大值
+# 时间复杂度：O(n)
+# 空间复杂度：O(n)
+class Solution2:
+    def trap(self, height: List[int]) -> int:
+        if len(height) <= 1:
+            return 0
 
-    def __init__(self, capacity: int):
-        self.dict = {}
-        self.capacity = capacity
-        self.dummy_head = ListNode(0, 0)
-        self.dummy_tail = ListNode(0, 0)
-        self.dummy_head.next = self.dummy_tail
-        self.dummy_tail.prev = self.dummy_head
+        max_left = [height[0]]
+        max_right = [height[len(height) - 1]]
+        res = 0
 
-    def get(self, key: int) -> int:
-        if key not in self.dict:
-            return -1
+        for i in range(1, len(height)):
+            left = max(height[i], max_left[i - 1])
+            max_left.append(left)
 
-        node = self.dict[key]
-        self.remove(node)
-        self.append(node)
+        reversed_height = list(reversed(height))
+        for i in range(1, len(reversed_height)):
+            right = max(reversed_height[i], max_right[0])
+            max_right.insert(0, right)
 
-        return node.val
+        for i in range(len(height)):
+            res += min(max_left[i], max_right[i]) - height[i]
 
-    def put(self, key: int, value: int) -> None:
-        if key in self.dict:
-            self.remove(self.dict[key])
+        return res
 
-        node = ListNode(key, value)
-        self.append(node)
-        self.dict[key] = node
 
-        if len(self.dict) > self.capacity:
-            head = self.dummy_head.next
-            self.remove(head)
-            del self.dict[head.key]
+# 双指针
+# 时间复杂度：O(n)
+# 空间复杂度：O(1)
+class Solution3:
+    def trap(self, height: List[int]) -> int:
+        if len(height) <= 1:
+            return 0
 
-    def remove(self, node: ListNode):
-        prev = node.prev
-        next = node.next
-        prev.next = next
-        next.prev = prev
+        left, right, res = 0, len(height) - 1, 0
 
-    def append(self, node: ListNode):
-        tail = self.dummy_tail.prev
-        tail.next = node
-        node.prev = tail
-        self.dummy_tail.prev = node
-        node.next = self.dummy_tail
+        max_left = height[0]
+        max_right = height[len(height) - 1]
+
+        while left <= right:
+            max_left = max(max_left, height[left])
+            max_right = max(max_right, height[right])
+
+            if max_left < max_right:
+                res += max_left - height[left]
+                left += 1
+            else:
+                res += max_right - height[right]
+                right -= 1
+
+        return res
 
 
 if __name__ == "__main__":
-    obj = LRUCache(2)
-    print(obj.get(1))
-    obj.put(1, 1)
-    obj.put(2, 2)
-    print(obj.get(1))
-    obj.put(3, 3)
-    print(obj.get(2))
-    obj.put(4, 4)
-    print(obj.get(1))
-    print(obj.get(3))
+    print(Solution().trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+    print(Solution2().trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+    print(Solution3().trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
