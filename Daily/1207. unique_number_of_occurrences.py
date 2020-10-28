b1b1fe4fabc586681d5ded5a705c5c889013eb43
Crisescode <zhaopanp2018@outlook.com>
2020-10-28 22:44:55 +0800

    [leetcode] update 1207

--- Daily/1207. unique_number_of_occurrences.py
+++ Daily/1207. unique_number_of_occurrences.py
@@ -21,4 +21,22 @@ from typing import List
 @timer
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        pass
+        if len(arr) <= 1:
+            return True
+
+        nums = {}
+        for n in arr:
+            if n in nums:
+                nums[n] += 1
+            else:
+                nums[n] = 1
+
+        counts = list(nums.values())
+        return len(counts) == len(set(counts))
+
+
+if __name__ == "__main__":
+    arr = [1, 2, 2, 1, 1, 3]
+    arr2 = [-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]
+    print(Solution().uniqueOccurrences(arr2))
+
