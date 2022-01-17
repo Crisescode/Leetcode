d8c4d507d7cf5ec20cf4e28036c8a0d70710fdc8
zhaopanpan <zhaopanpan@xinye.com>
2022-01-17 10:25:00 +0800

    [ftr] update 160.

--- Python/LinkedList/160. intersection_of_two_linked_lists.py
+++ Python/LinkedList/160. intersection_of_two_linked_lists.py
@@ -3,18 +3,22 @@
 
 # https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
 """
-Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+Given the heads of two singly linked-lists headA and headB, return the node at which the
+two lists intersect. If the two linked lists have no intersection at all, return null.
 
 For example, the following two linked lists begin to intersect at node c1:
 
 ...
 """
 
+from typing import Union
 from utils import ListNode
 
 
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+    def getIntersectionNode(
+            self, headA: ListNode, headB: ListNode
+    ) -> Union[ListNode, None]:
         linkedList_A = []
         A, B = headA, headB
         while headA:
@@ -30,29 +34,33 @@ class Solution:
 
 
 class Solution2:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+    def getIntersectionNode(
+            self, headA: ListNode, headB: ListNode
+    ) -> ListNode:
         A, B = headA, headB
-        while A.val != B.val:
+        while A != B:
             A = A.next if A else headB
             B = B.next if B else headA
 
-        return A.val
+        return A
 
 
 if __name__ == "__main__":
-    l3 = l3_1 = ListNode(0)
-    l3_1.next = ListNode(1)
-    l3_1 = l3_1.next
-    l3_1.next = ListNode(2)
-    l3_1 = l3_1.next
-    l3_1.next = ListNode(3)
+    l1_copy = l1 = ListNode(0)
+    l1.next = ListNode(1)
+    l1.next.next = ListNode(2)
+    l1.next.next.next = ListNode(3)
+    l1.next.next.next.next = ListNode(5)
 
-    l1 = l1_1 = ListNode(5)
-    l1_1.next = ListNode(4)
-    l1_1 = l1_1.next
-    l1_1.next = ListNode(2)
-    l1_1 = l1_1.next
-    l1_1.next = ListNode(3)
+    l2_copy = l2 = ListNode(5)
+    l2.next = ListNode(4)
+    l2.next.next = ListNode(10)
+    l2.next.next.next = ListNode(2)
+    l2.next.next.next.next = ListNode(1)
+
+    print("l1: ", l1)
+    print("l1 copy: ", l1_copy)
+    print("l2: ", l2)
 
     # print(Solution().getIntersectionNode(l1, l3))
-    print(Solution2().getIntersectionNode(l1, l3))
+    print(Solution2().getIntersectionNode(l1_copy, l2_copy))
