c33990798a1a6ff21044e088d315ae33efdb6fd0
Crisescode <zhaopanp2018@outlook.com>
2022-01-18 00:22:11 +0800

    [148] to do.

---
+++ Python/LinkedList/148. sort_list.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# https://leetcode-cn.com/problems/sort-list/
+# Given the head of a linked list, return the list after sorting it in ascending order.
+#
+
+from typing import Optional
+from utils import ListNode
+
+
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+
+        mid = ()
+
+    def mergeSort(self, node: ListNode, left: int, right: int) -> ListNode:
+        pass
