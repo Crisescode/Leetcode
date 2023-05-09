58d1a587a6a7288c74ef3beee0f1fff79bebc718
Crisescode <crise.zhaopp@gmail.com>
2023-05-09 20:34:16 +0800

    ugo

--- Go/Array/152. 乘积最大子数组.go
+++ Go/Array/152. 乘积最大子数组.go
@@ -1,11 +1,26 @@
 package main
 
+import "fmt"
+
 func maxProduct(nums []int) int {
 
 	res := 0
 	for i := 0; i < len(nums); i++ {
 		for j := i; i < len(nums); i++ {
-			for p := 
+			s := 1
+			for p := i; p < j; p++ {
+				s *= nums[p]
+			}
+
+			if s > res {
+				res = s
+			}
 		}
 	}
+
+	return res
+}
+
+func main() {
+	fmt.Print(maxProduct([]int{2, 3, -2, 4}))
 }
